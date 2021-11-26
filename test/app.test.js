5410eeee56510e88ac9d5ce65f38da5800702247
Vadzim Antonau <vanet@tut.by>
2021-11-26 19:38:36 +0300

    refactor: rewrite test

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const http = require('http');
-var myApp = require('../app.js');
-var request = require('supertest')(`http://localhost:3000/person`);
+const myApp = require('../app.js');
+const request = require('supertest')(`http://localhost:3000/person`);
 
 const createData = { name: 'Vadzim', age: 34, hobbies: [] };
 const updateData = { name: 'Vadzim', age: 34, hobbies: ['programming'] };
@@ -9,75 +9,50 @@ const updateData = { name: 'Vadzim', age: 34, hobbies: ['programming'] };
 describe('Hacker scope - E2E API Tests', function () {
   describe('First scenario', function () {
     let personId;
-    it('should return empty array of persons', function (done) {
-      request.get('/').expect(200, [], done);
+    it('should return empty array of persons', async () => {
+      await request.get('/').expect(200, []);
     });
 
-    it('should return created object of person with id', function (done) {
-      request
-        .post('/')
-        .send(createData)
-        .expect(201)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body).toEqual(
-            expect.objectContaining({ id: expect.any(String), ...createData })
-          );
-          personId = res.body.id;
-          done();
-        });
+    it('should return created object of person with id', async () => {
+      const res = await request.post('/').send(createData);
+      expect(res.status).toEqual(201);
+      expect(res.body).toEqual(
+        expect.objectContaining({ id: expect.any(String), ...createData })
+      );
+      personId = res.body.id;
     });
 
-    it('should return object of person created in prev test', function (done) {
-      request
-        .get('/' + personId)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body).toEqual(
-            expect.objectContaining({ id: personId, ...createData })
-          );
-          done();
-        });
+    it('should return object of person created in prev test', async () => {
+      const res = await request.get('/' + personId);
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual(
+        expect.objectContaining({ id: personId, ...createData })
+      );
     });
 
-    it('should return updated object of person', function (done) {
-      request
-        .put('/' + personId)
-        .send(updateData)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body).toEqual(
-            expect.objectContaining({ id: personId, ...updateData })
-          );
-          done();
-        });
+    it('should return updated object of person', async () => {
+      const res = await request.put('/' + personId).send(updateData);
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual(
+        expect.objectContaining({ id: personId, ...updateData })
+      );
     });
 
-    it('should delete object of person by id and return 204', function (done) {
-      request
-        .delete('/' + personId)
-        .expect(204)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body).toEqual('');
-          done();
-        });
+    it('should delete object of person by id and return 204', async () => {
+      const res = await request.delete('/' + personId);
+      console.log(personId);
+      console.log(res.body);
+      expect(res.status).toEqual(204);
+      expect(res.body).toEqual('');
     });
 
-    it('should return 404 and msg person is not found', function (done) {
-      request
-        .get('/' + personId)
-        .expect(404)
-        .end((err, res) => {
-          if (err) return done(err);
-          console.log(res.body);
-          expect(res.body).toEqual(
-            expect.objectContaining({ id: personId, ...createData })
-          );
-          done();
-        });
+    it('should return 404 and msg person is not found', async () => {
+      const res = await request.get('/' + personId);
+      console.log(res.body);
+      expect(res.status).toEqual(404);
+      expect(res.body).toEqual({
+        error: `Person with id ${personId} not found!`,
+      });
     });
   });
 });
